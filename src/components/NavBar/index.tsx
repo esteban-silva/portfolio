5edf2b2864c0f2918283a576b13ea581f8d86e74
esteban <esteban_silva11@hotmail.com>
2024-05-22 13:14:07 -0300

    intento de arreglar navbar 2

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -27,7 +27,7 @@ const NavBar = () => {
 
   return (
     <header className="fixed top-0 z-10 flex items-center justify-center w-full mx-auto mt-2">
-      <nav className="flex px-3 text-sm font-medium rounded-full text-gray-600 dark:text-gray-200 justify-center items-center">
+      <nav id="header-nav" className="flex px-3 text-sm font-medium rounded-full text-gray-600 dark:text-gray-200 justify-center items-center">
         {navItems.map((link) => (
           <a
             className="relative block px-2 py-2 transition hover:text-yellow-500 dark:hover:text-yellow-400"
